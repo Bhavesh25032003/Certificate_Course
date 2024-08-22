937454a5ef53c5581d5b1e098b40f40bd122ae3b
Bhavesh25032003 <bhaveshvinayaraj2003@gmail.com>
2024-08-22 16:31:49 +0530

    Login Commit

--- React/app/src/App.js
+++ React/app/src/App.js
@@ -1,9 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './Home';
+import {useState} from 'react';
 
 function App() {
+  let firstname = "Bhavesh";
+  const [email,setEmail] = useState('youremail');
+  const [mobile,setMobile] = useState('yourmobile');
+  function ValiditeState(){
+    if(email == ""){
+        alert("E-mail is Empty")
+    }else if(mobile == ""){
+      alert("Mobile is Empty")
+    }else{
+      alert("Everything is Fine")
+    }
+  }
   return (
-    <h1>This Is React App </h1>
+    <div className="App"> 
+      <h1>This Is React App </h1>
+      {firstname} <br/>
+      {email} <br/>
+      {mobile} <br/>
+      <Home/>
+      <input type="text" onChange={(e) => setEmail(e.target.value)}/> <br/>
+      <input type="text" onChange={(e) => setMobile(e.target.value)}/> <br/>
+      <button onClick={() => ValiditeState()}>Click</button>
+    </div>
+
   );
 }
 
