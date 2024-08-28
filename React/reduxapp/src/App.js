2666937644010a1932765bc07da96ba4408c1f7c
Bhavesh25032003 <bhaveshvinayaraj2003@gmail.com>
2024-08-28 16:38:33 +0530

    React commit

---
+++ React/reduxapp/src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+import {createBrowserRouter, RouterProvider,} from "react-router-dom";
+import Home from './Home';
+import About from './About';
+import Contact from './Contact';
+import Header from './header';
+import { myStore } from './redux/config';
+import { Provider } from 'react-redux';
+
+let routerPaths = createBrowserRouter([
+  {"path":"/home","element":<Home/>},
+  {"path":"/about","element":<About/>},
+  {"path":"/contact","element":<Contact/>},
+])
+function App() {
+  return (
+    <Provider store = {myStore}>
+      <div>
+          <RouterProvider router = {routerPaths} />
+          <Header/>
+      </div>
+    </Provider>
+  );
+}
+
+export default App;
---
+++ React/reduxapp/src/header.js
@@ -0,0 +1,9 @@
+export default function Header(){
+    return(
+        <ul>
+            <li><a href="/home">HOME</a></li>
+            <li><a href="/about">ABOUT</a></li>
+            <li><a href="/contact">CONTACT</a></li>
+        </ul>
+    )
+}
